cf38db1ed96c440fa8eea5ae9389fc544dd78d3b
sarvind1119 <sarvind.11193@gmail.com>
2024-05-28 14:34:39 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,180 @@
+#PLEASE check main.py file before going through this
+
+#Import necessary libraries
+from langchain.embeddings.openai import OpenAIEmbeddings
+import os
+from pinecone import ServerlessSpec
+#from main import *
+from pinecone import Pinecone
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+from openai import OpenAI
+# initialize connection to pinecone (get API key at app.pinecone.io)
+api_key = os.environ.get('PINECONE_API_KEY')
+pc = Pinecone(api_key=api_key)
+from pinecone import ServerlessSpec
+
+cloud = 'aws'
+region = 'us-east-1'
+
+spec = ServerlessSpec(cloud=cloud, region=region)
+
+index_name = 'dolr'
+
+# get openai api key from platform.openai.com
+OPENAI_API_KEY =  os.environ.get('OPENAI_API_KEY')
+
+model_name = 'text-embedding-ada-002'
+
+embed = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=OPENAI_API_KEY,
+    
+)
+
+from langchain.vectorstores import Pinecone
+
+text_field = "text"
+
+# switch back to normal index for langchain
+index = pc.Index(index_name)
+
+vectorstore = Pinecone(
+    index, embed.embed_query, text_field
+)
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+def ask_and_get_answer(vector_store, q, k=3):
+    from langchain.chains import RetrievalQA
+    from langchain_openai import ChatOpenAI
+
+    # Initialize the language model with the specified parameters.
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0 )
+
+    # Set up the retriever with the given vector store and search parameters.
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
+
+    # Create a retrieval-based QA chain that returns the source documents along with the answers.
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
+    # Invoke the chain with the provided question and get the response.
+    answer = chain.invoke(q)
+
+    # Print the result from the answer.
+    print(answer['result'])
+
+    # Print reference information.
+    print('Reference:\n')
+    # for doc in answer["source_documents"]:
+    #     raw_dict = doc.metadata
+    #     print("Page number:", raw_dict['page'], "Filename:", raw_dict['source'])
+    for x in range(len(answer["source_documents"][0].metadata)):
+        raw_dict = answer["source_documents"][x].metadata
+        print("Page number:", raw_dict['page'], "Filename:", raw_dict['source'])
+
+    # If needed, return the answer object.
+    return answer
+
+# completion llm
+llm = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+)
+
+from langchain.chains import RetrievalQAWithSourcesChain
+
+qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+)
+import streamlit as st
+
+# Sidebar contents
+with st.sidebar:
+    st.title('💬 LLM Chat App on Department of Land Resources (DOLR)(Documents)...')
+
+        # Adding the "Developed by xyz" line in dark green color
+    st.markdown('''
+    <div style="color: red;">
+    Developed by Shreya Shree
+    </div>
+    ''', unsafe_allow_html=True)
+# Adding the new list with green bullet points
+    st.markdown('''
+    <div style="color: green;">
+    <ul>
+        <li>Bhoomi Sampan Certification.pdf</li>
+        <li>Bhu-Aadhar_ Unique Land Parcel Identification Nu...pdf</li>
+        <li>Cactus.pdf</li>
+        <li>Common Guidelines 2008.pdf</li>
+        <li>Compensation, Rehabilitation, Resettlement, Development plan rules.pdf</li>
+        <li>DILRMP website Intro.pdf</li>
+        <li>Draft Operational Guidelines for DILRMP.pdf</li>
+        <li>Guidelines for evaluation of preparatory phase of IWMP.pdf</li>
+        <li>Guidelines for livelihood component WDC 2.pdf</li>
+        <li>Guidelines, Technical Manual & MIS 2018-19.pdf</li>
+        <li>Impact Assessment study of Bhoomi Sampan districts.pdf</li>
+        <li>Land acquisition (Companies) rules 1963.pdf</li>
+        <li>Land Acquisition Act 1894.pdf</li>
+        <li>Linkage of e-Court with Land Records_2F Registrati...pdf</li>
+        <li>Livelihood Production Systems and Microenterprises - IWMP Operational Guidelines.pdf</li>
+        <li>MoRDEnglish_AR2012_13_0.pdf</li>
+        <li>National Generic Document Registration System (NG...pdf</li>
+        <li>National Rehabilitation and Resettlement Policy 2007 — OCR reqd.pdf</li>
+        <li>Registration Act 1908 - Bare Act.pdf</li>
+        <li>Revenue Court Case Management System.pdf</li>
+        <li>REWARD ToR for Fin Mgmt Expert.pdf</li>
+        <li>REWARDS.pdf</li>
+        <li>RFCTLARR 2014 ordinance — OCR reqd.pdf</li>
+        <li>RFCTLARR 2015 - corrigendum to second amendment.pdf</li>
+        <li>RFCTLARR 2015 - second amendment as intro in LS.pdf</li>
+        <li>RFCTLARR 2015 Amendment — intro in LS.pdf</li>
+        <li>RFCTLARR 2015 Ordinance.pdf</li>
+    </ul>
+    </div>
+
+    Training on "Wasteland Atlas of India 2019" in progress
+    ''', unsafe_allow_html=True)
+
+    # Add vertical space
+    st.markdown('''
+    ---
+
+    **In case of suggestions/feedback/Contributions please reach out to:**
+    [NIC Training Unit @ nictu@lbsnaa.gov.in]
+    ''')
+
+
+def display_answer(answer):
+    st.write("### Query")
+    st.write(answer['query'])
+
+    st.write("### Result")
+    result = answer['result'].replace('\n', '  \n')  # Ensuring markdown line breaks
+    st.markdown(result)
+
+    if "source_documents" in answer:
+        st.write("### Reference Documents")
+        for i, doc in enumerate(answer["source_documents"], start=1):
+            st.write(f"#### Document {i}")
+            st.write(f"**Page number:** {doc.metadata['page']}")
+            st.write(f"**Source file:** {doc.metadata['source']}")
+            content = doc.page_content.replace('\n', '  \n')  # Ensuring markdown line breaks
+            st.markdown(content)
+
+def main():
+    st.title("Question and Answering App powered by LLM and Pinecone on Department of Land Resources (DOLR) ")
+    text_input = st.text_input("Ask your query...") 
+
+    if st.button("Ask Query"):
+        if len(text_input) > 0:
+            answer = ask_and_get_answer(vectorstore, text_input)
+            display_answer(answer)
+
+# The main function call
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,45 @@
+
+import openai
+import langchain
+import pinecone
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def read_doc(directory):
+    file_loader=PyPDFDirectoryLoader(directory)
+    documents=file_loader.load()
+    return documents
+
+doc=read_doc('Docs/')
+#len(doc)
+
+def chunk_data(docs,chunk_size=800,chunk_overlap=50):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
+    doc=text_splitter.split_documents(docs)
+    return docs
+
+documents=chunk_data(docs=doc)
+len(documents)
+
+## Embedding Technique Of OPENAI
+embeddings=OpenAIEmbeddings(api_key=os.environ['OPENAI_API_KEY'])
+
+# vectors=embeddings.embed_query("How are you?")
+# len(vectors)
+
+from langchain_pinecone import PineconeVectorStore
+
+vectorstore_from_docs = PineconeVectorStore.from_documents(
+    documents,
+    index_name='dolr',
+    embedding=embeddings
+)
+# index_name='business'
+# vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
+# vectorstore.add_documents(doc)
