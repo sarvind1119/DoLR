c054de15da4b4704ecf4a7e1a50d22b24237111d
sarvind1119 <69563525+sarvind1119@users.noreply.github.com>
2024-05-28 16:04:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ with st.sidebar:
 
     st.markdown('''
     ## About
-    This GPT helps in answering questions related to Allocation & Transaction of Business Rules(using the following Documents)
+    This GPT helps in answering questions related to Department of Land Resources (DOLR)(Documents)(using the following Documents)
 
     [Documents Repository](https://drive.google.com/drive/folders/1-XT5Exfsspr9Mb5JE9_79M5QwBLlUeG8)
     <div style="color: red;">
