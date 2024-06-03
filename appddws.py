cc0677048b3e113b1c60fa4c1ea35bb87d4afe19
sarvind1119 <sarvind.11193@gmail.com>
2024-06-03 15:15:45 +0530

    ddws

--- appddws.py
+++ appddws.py
@@ -1,98 +1,46 @@
-#PLEASE check main.py file before going through this
-
-#Import necessary libraries
-from langchain.embeddings.openai import OpenAIEmbeddings
-import os
-from pinecone import ServerlessSpec
-#from main import *
-from pinecone import Pinecone
-import streamlit as st
-import warnings
-warnings.filterwarnings('ignore')
-from openai import OpenAI
-# initialize connection to pinecone (get API key at app.pinecone.io)
-api_key = os.environ.get('PINECONE_API_KEY')
-pc = Pinecone(api_key=api_key)
-from pinecone import ServerlessSpec
-
-cloud = 'aws'
-region = 'us-east-1'
-
-spec = ServerlessSpec(cloud=cloud, region=region)
-
-index_name = 'ddws'
-
-# get openai api key from platform.openai.com
-OPENAI_API_KEY =  os.environ.get('OPENAI_API_KEY')
-
-model_name = 'text-embedding-ada-002'
-
-embed = OpenAIEmbeddings(
-    model=model_name,
-    openai_api_key=OPENAI_API_KEY,
-    
-)
-
-from langchain.vectorstores import Pinecone
-
-text_field = "text"
-
-# switch back to normal index for langchain
-index = pc.Index(index_name)
-
-vectorstore = Pinecone(
-    index, embed.embed_query, text_field
-)
-
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-def ask_and_get_answer(vector_store, q, k=3):
-    from langchain.chains import RetrievalQA
-    from langchain_openai import ChatOpenAI
-
-    # Initialize the language model with the specified parameters.
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0 )
-
-    # Set up the retriever with the given vector store and search parameters.
-    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
-
-    # Create a retrieval-based QA chain that returns the source documents along with the answers.
-    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-
-    # Invoke the chain with the provided question and get the response.
-    answer = chain.invoke(q)
-
-    # Print the result from the answer.
-    print(answer['result'])
-
-    # Print reference information.
-    print('Reference:\n')
-    # for doc in answer["source_documents"]:
-    #     raw_dict = doc.metadata
-    #     print("Page number:", raw_dict['page'], "Filename:", raw_dict['source'])
-    for x in range(len(answer["source_documents"][0].metadata)):
-        raw_dict = answer["source_documents"][x].metadata
-        print("Page number:", raw_dict['page'], "Filename:", raw_dict['source'])
-
-    # If needed, return the answer object.
-    return answer
-
-# completion llm
-llm = ChatOpenAI(
-    openai_api_key=OPENAI_API_KEY,
-    model_name='gpt-3.5-turbo',
-    temperature=0.0
-)
-
-from langchain.chains import RetrievalQAWithSourcesChain
-
-qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vectorstore.as_retriever()
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
 )
 import streamlit as st
+from streamlit_chat import message
+from utilsddws import *
 
+st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=os.environ.get('OPENAI_API_KEY'))
+
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
+and if the answer is not contained within the text below, say 'I don't know. ALWAYS give results in bullets points. '""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
+# Sidebar contents
 # Sidebar contents
 with st.sidebar:
     st.title('💬 LLM Chat App on Department of Drinking Water and Sanitation....')
@@ -138,34 +86,46 @@ with st.sidebar:
     **In case of suggestions/feedback/Contributions please reach out to:**
     [NIC Training Unit @ nictu@lbsnaa.gov.in]
     ''')
+    # Add vertical space
+    st.markdown('''
+    ---
+
+    **In case of suggestions/feedback/Contributions please reach out to:**
+    [NIC Training Unit @ nictu@lbsnaa.gov.in]
+    ''')
 
 
-def display_answer(answer):
-    st.write("### Query")
-    st.write(answer['query'])
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            # st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = ask_and_get_answer(vectorstore,refined_query)
+            # print(context)  
+            response = conversation.predict(input=f"Context://n {context} //n//n Query://n{query}")
+            st.session_state.requests.append(query)
+            st.session_state.responses.append(response)
 
-    st.write("### Result")
-    result = answer['result'].replace('\n', '  \n')  # Ensuring markdown line breaks
-    st.markdown(result)
+            if "source_documents" in context:
+                st.write("### Reference Documents")
+                for i, doc in enumerate(context['source_documents'], start=1):
+                    st.write(f"#### Document {i}")
+                    st.write(f"**Page number:** {doc.metadata['page']}")
+                    st.write(f"**Source file:** {doc.metadata['source']}")
+                    content = doc.page_content.replace('//n', '  //n')  # Ensuring markdown line breaks
+                    st.markdown(content)
 
-    if "source_documents" in answer:
-        st.write("### Reference Documents")
-        for i, doc in enumerate(answer["source_documents"], start=1):
-            st.write(f"#### Document {i}")
-            st.write(f"**Page number:** {doc.metadata['page']}")
-            st.write(f"**Source file:** {doc.metadata['source']}")
-            content = doc.page_content.replace('\n', '  \n')  # Ensuring markdown line breaks
-            st.markdown(content)
 
-def main():
-    st.title("Question and Answering App powered by LLM and Pinecone on Department of Drinking Water and Sanitation ")
-    text_input = st.text_input("Ask your query...") 
 
-    if st.button("Ask Query"):
-        if len(text_input) > 0:
-            answer = ask_and_get_answer(vectorstore, text_input)
-            display_answer(answer)
 
-# The main function call
-if __name__ == "__main__":
-    main()
+with response_container:
+    if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
